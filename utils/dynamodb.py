5554090c1d48d99fb50a1ba9ea384b2f7194f27b
Jinwoo <139137529+hijinwoo@users.noreply.github.com>
2024-02-01 22:41:05 +0900

    feat : 번역 기능 요청 시 error 발생으로 인한 code 추가 & 수정

--- utils/dynamodb.py
+++ utils/dynamodb.py
@@ -1,6 +1,5 @@
 import boto3
 import json
-import sys
 import json
 
 secret_name = "secret/aws"
@@ -14,22 +13,19 @@ secret = json.loads(get_secret_value_response['SecretString'])
 mongodb_sw = False
 dynamodb = boto3.client('dynamodb', region_name='ap-northeast-2', aws_access_key_id = secret["aws.accessKey"], aws_secret_access_key = secret["aws.secretKey"])
 
-table_name = "test"    # 나중에 table_name 바꾸면 수정해주기.
+table_name = "restaurant"    # 나중에 table_name 바꾸면 수정해주기.
+def get_restaurants():
+    # 테이블 스캔하여 모든 아이템 가져오기
+    response = dynamodb.scan(TableName=table_name)
+    # items = response['Items'] if mongodb_sw else response['Items'][0]
+    return response['Items']
 
-# 테이블 스캔하여 모든 아이템 가져오기
-response = dynamodb.scan(TableName=table_name)
-
-# items = response['Items'] if mongodb_sw else response['Items'][0]
-items = response['Items']
-# print("testest", items)
-def get_restaurant_info(restaurant_name):
-    # if key["L"]["name"] == restaurant_name
-    if mongodb_sw:
-        pass
-    else:
+def find_restaurant(restaurant_name: str) -> dict or None:
+    if not mongodb_sw:
+        items = get_restaurants()
         for item in items:
-            for key, value in item.items():
-                if (key=="name") and (value["S"] == restaurant_name):
+            if type(item) == dict and "name" in item:
+                if type(item["name"]) == dict and item["name"].get("S", "") == restaurant_name:
                     return item
-                
+    return None
     
