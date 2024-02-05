29d7a839f50dc95482168896cf7b15edfc4ea940
Jinwoo <139137529+hijinwoo@users.noreply.github.com>
2024-02-05 12:16:10 +0900

    feat : exchange_money.py code 수정

--- awstrans.py
+++ awstrans.py
@@ -64,10 +64,9 @@ def api_for_id(restaurant_id):
 @app.route('/api/v1/record', methods=['GET'])
 @log_function_execution_time
 def api_for_name(restaurant_name: str):
-    target_language = request.headers.get("Language")
+    target_language = request.headers.get("Language", "en")
     restaurant_info = find_restaurant(restaurant_name)
     translated_store = translate_store_info(restaurant_info, target_language) # target language는 프론트에서 header에 담겨져 온다
-
     if not translated_store:
         return "No Content", 204
     return translated_store, 200
--- utils/exchange_money.py
+++ utils/exchange_money.py
@@ -1,27 +1,27 @@
-import re
-from forex_python.converter import CurrencyRates
-
+import json
 from utils.logger import logged
+import boto3
 
-NATION_DICT = {'eng': 'USD', 'jp': 'JPY'}
+secret_name = "secret/aws"
+region_name = "ap-northeast-2"
+table_name = "exchange_late"
+session = boto3.session.Session()
+client = session.client(service_name='secretsmanager',region_name=region_name)
+get_secret_value_response = client.get_secret_value(SecretId=secret_name)
+secret = json.loads(get_secret_value_response['SecretString'])
+
+dynamodb = boto3.resource('dynamodb', region_name='ap-northeast-2', aws_access_key_id = secret["aws.accessKey"], aws_secret_access_key = secret["aws.secretKey"])
+
 
 @logged
 def exchange(price, target_language):
-    c = CurrencyRates()
-    exchange_rate_usd_to_krw = c.get_rate(NATION_DICT.get(target_language),'KRW')
-    return round(float(price) / exchange_rate_usd_to_krw, 2)
-
-@logged
-def exchange_str_price(str_price):
-    price = str_price
-    #extract_numbers(str_price) if type(str_price) == str else str_price
-    #if len(price) > 0:
-    return exchange(int(price))
-    #return price
-
-@logged
-def extract_numbers(text):
-    # 정규식을 사용하여 숫자만 추출
-    numbers = re.findall(r'\d+', text)
-    # 추출된 숫자들을 문자열로 변환하고 모두 붙여서 반환
-    return ''.join(numbers)
+    table = dynamodb.Table("exchange_late")
+    # DynamoDB에서 항목 조회
+    response = table.get_item(
+        Key={
+            'id': '1'
+        }
+    )
+    item = response.get('Item')
+    late = float(item.get(target_language))
+    return round(float(price) * late, 2)
